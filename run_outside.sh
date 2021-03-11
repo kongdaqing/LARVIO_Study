f7902da5f42b0258b7f9845a2c8bcdefcad76b7b
sniper1204 <274960485@qq.com>
2021-03-11 19:27:03 +0800

    new shell

---
+++ run_outside.sh
@@ -0,0 +1,1 @@
+~/yangzheng/LARVIO/build/larvio ~/yangzheng/bag/outside/imu_data.csv ~/yangzheng/bag/outside/image_time.csv ~/yangzheng/bag/outside/image ~/yangzheng/LARVIO/config/d435i_640_480.yaml
